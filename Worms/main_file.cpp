683ffd531cc3585b5f340a8e9c86379c1f905dc1
starspire <krzysztof.wedrowicz@wp.pl>
2015-05-22 14:26:46 +0200

    loading obj progress

--- Worms/main_file.cpp
+++ Worms/main_file.cpp
@@ -6,20 +6,25 @@
 #include <stdio.h>
 #include <vector>
 #include <fstream>
+#include <iostream>
 #include <string>
 
 using namespace std;
 using namespace glm;
 
-float *geomVertices, *geomColors;
+float *geomVertices, *geomColors, *geomNormals;
+unsigned int *geomFaces;
 int geomVertexCount = 0;
+int it = 0;
+int licz_fejsy = 0;
 
-float speed = 60; //60 stopni/s
+float speed = 6; //60 stopni/s
 int lastTime = 0;
 float angle_float;
+float scaleModifier = 0;
 
 //bool loadOBJ(const char *path, std::vector<glm::vec3> &out_vertices, std::vector<glm::vec2> &out_uvs, std::vector<glm::vec3> &out_normals)
-bool loadOBJ(const char *path, std::vector<float> &out_vertices, std::vector<float> &out_colors, int &vertex_count)
+bool loadOBJ(const char *path, std::vector<float> &out_vertices, std::vector<float> &out_colors, std::vector<float> &out_normals, std::vector<unsigned int> &out_faces, int &vertex_count)
 {
 	std::fstream plik;
 	plik.open(path, std::ios::in);
@@ -27,10 +32,14 @@ bool loadOBJ(const char *path, std::vector<float> &out_vertices, std::vector<flo
 		return false;
 
 	std::string buf;
-	float fbuf;
+	float fbuf; 
+	int intbuf;
+	it=0;
+	bool guard = 1;
 	while (!plik.eof())
 	{
-		plik >> buf;
+		if (guard)
+			plik >> buf;
 		if (buf.compare("v") == 0)
 		{
 			plik >> fbuf;
@@ -44,6 +53,39 @@ bool loadOBJ(const char *path, std::vector<float> &out_vertices, std::vector<flo
 			out_colors.push_back(0.0f);
 			vertex_count++;
 			//		printf("%d\n", vertex_count);
+			guard = 1;
+		}
+		else if (buf.compare("vn") == 0)
+		{
+			plik >> fbuf;
+			out_normals.push_back(fbuf);
+			plik >> fbuf;
+			out_normals.push_back(fbuf);
+			plik >> fbuf;
+			out_normals.push_back(fbuf);
+			guard = 1;
+		}
+		else if (buf.compare("f") == 0)
+		{
+			int indexa, indexb;
+			while(1)
+			{
+				plik >> buf;
+				if (buf.compare("f") == 0)
+				{
+					guard = 0;
+					break;
+				}
+				else if (buf.compare("o") == 0 || plik.eof()) break;
+
+				licz_fejsy += 3;
+				sscanf_s(buf.c_str(), "%d//%d", &indexa, &indexb);
+				out_faces.push_back(indexa);
+				out_faces.push_back(it);
+				out_faces.push_back(indexb);
+				guard = 1;
+			}
+			it++;
 		}
 	}
 
@@ -56,7 +98,7 @@ void displayFrame(void) {
 	glClearColor(0, 0, 0, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	mat4 P = perspective(17.0f, 1.0f, 1.0f, 50.0f);
+	mat4 P = perspective(1.5f, 1.0f, 1.0f, 50.0f);
 	mat4 V = lookAt(vec3(0.0f, 0.0f, -5.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
 	mat4 M = mat4(1.0);
 
@@ -64,15 +106,19 @@ void displayFrame(void) {
 	glLoadMatrixf(value_ptr(P));
 	glMatrixMode(GL_MODELVIEW);
 
+	M = rotate(M, angle_float, vec3(0.0f, 1.0f, 0.0f));
+	M = scale(M, vec3(scaleModifier, scaleModifier, scaleModifier));
 	glLoadMatrixf(value_ptr(V*M));
 
 	glEnableClientState(GL_VERTEX_ARRAY);
-	glEnableClientState(GL_COLOR_ARRAY);
+	//glEnableClientState(GL_COLOR_ARRAY);
 	glVertexPointer(3, GL_FLOAT, 0, geomVertices);
-	glColorPointer(3, GL_FLOAT, 0, geomColors);
-	glDrawArrays(GL_TRIANGLES, 0, geomVertexCount);
+//	glColorPointer(3, GL_FLOAT, 0, geomColors);
+	glNormalPointer(3, GL_FLOAT, geomNormals);
+	glDrawElements(GL_TRIANGLES, licz_fejsy, GL_UNSIGNED_INT, geomFaces);
+	//glDrawArrays(GL_TRIANGLES_ADJACENCY, 0, geomVertexCount);
 	glDisableClientState(GL_VERTEX_ARRAY);
-	glDisableClientState(GL_COLOR_ARRAY);
+	//glDisableClientState(GL_COLOR_ARRAY);
 	glutSwapBuffers();
 
 }
@@ -105,18 +151,53 @@ void initializeGLEW() {
 	fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
 }
 
+void keyDown(int c, int x, int y)
+{
+	if (c == GLUT_KEY_LEFT)
+	{
+		speed = -6;
+	}
+	else if (c == GLUT_KEY_RIGHT)
+	{
+		speed = 6;
+	}
+	else if (c == GLUT_KEY_UP)
+	{
+		scaleModifier += 0.5f;
+	}
+	else if (c == GLUT_KEY_DOWN)
+	{
+		scaleModifier -= 0.5f;
+	}
+}
+
+
+
+void keyUp(int c, int x, int y)
+{
+	if (c == GLUT_KEY_LEFT || c == GLUT_KEY_RIGHT)
+	{
+		speed = 0;
+	}
+}
+
 int main (int argc, char** argv) {
 
 	initializeGLUT(&argc, argv);
 	initializeGLEW();
 	//Kod inicjuj¹cy tutaj
 	glutIdleFunc(nextFrame);
+	glutSpecialFunc(keyDown);
+	glutSpecialUpFunc(keyUp);
 
-	std::vector<float> dupa1, dupa2;
-	bool alejaja = loadOBJ("robot.obj", dupa1, dupa2, geomVertexCount);
+	std::vector<float> dupa1, dupa2, dupa3;
+	std::vector<unsigned int> dupa4;
+	bool alejaja = loadOBJ("robot.obj", dupa1, dupa2, dupa3, dupa4,geomVertexCount);
 	if (!alejaja) return 1;
 	geomVertices = &dupa1[0];
 	geomColors = &dupa2[0];
+	geomNormals = &dupa3[0];
+	geomFaces = &dupa4[0];
 
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
