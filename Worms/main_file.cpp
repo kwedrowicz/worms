4567d35314ac882f13d1bec19549f93e0ba55c00
starspire <krzysztof.wedrowicz@wp.pl>
2015-06-22 18:34:43 +0200

    changes

--- Worms/main_file.cpp
+++ Worms/main_file.cpp
@@ -74,7 +74,9 @@ void nextFrame(void) {
 	robots[active].ball.M = rotate(robots[active].ball.M,-1*robots[active].direction * speed/60, vec3(0.0, 0.0, 1.0));
 	for (int i = 0; i < robots.size(); i++)
 	{
-		float distance = wall.HowFarFromSurface(robots[i].body.M2, vec4((robots[i].body.boundingBox.bottomLeft.x + robots[i].body.boundingBox.bottomRight.x) / 2.0f, robots[i].body.boundingBox.bottomRight.y, 0.0f, 0.0f));
+		Model body = robots[i].body;
+		body.boundingBox = body.boundingBox * robots[i].body.M2;
+		float distance = wall.HowFarFromSurface(vec4((body.boundingBox.bottomLeft.x + body.boundingBox.bottomRight.x) / 2.0f, body.boundingBox.bottomRight.y, 0.0f, 0.0f));
 		cout << distance << endl;
 		if (distance > 0.0f)
 		{
